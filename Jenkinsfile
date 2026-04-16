87ff46b87bafc115e336de7d7453b1d3999cd2f7
stutimahesh <stuti05mahesh@gmail.com>
2026-04-16 17:25:22 +0530

    Updated Jenkinsfile

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,31 +1,44 @@
-pipeline { 
-agent any    // Use any available agent 
-tools { 
-maven 'Maven'  // Ensure this matches the name configured in Jenkins 
-} stages { 
-stage('Checkout') { 
-steps { 
-git branch: 'master', url: 'https://github.com/stutimahesh/MySeleniumRepo02.git' 
-} 
-} stage('Build') {
-steps { 
-sh 'mvn clean package' // Run Maven build
-} 
-} stage('Test') { 
-steps { 
-sh 'mvn test' // Run unit tests
-} 
-} stage('Run Application') { 
-steps { 
-// Start the JAR application 
-sh 'java -jar target/MyMavenSeleniumApp01-1.0-SNAPSHOT.jar' 
-} 
-}
-} post { 
-success { 
-echo 'Build and deployment successful!' 
-} failure { 
-echo 'Build failed!' 
-}
-}
+pipeline {
+    agent any
+
+    tools {
+        maven 'Maven'
+    }
+
+    stages {
+
+        stage('Checkout') {
+            steps {
+                git branch: 'master',
+                    url: 'https://github.com/stutimahesh/MySeleniumRepo02.git',
+                    credentialsId: 'GitHubCredential'
+            }
+        }
+
+        stage('Build') {
+            steps {
+                sh 'mvn clean package'
+            }
+        }
+
+        stage('Test') {
+            steps {
+                sh 'mvn test'
+            }
+        }
+        stage('Run') {
+            steps {
+                sh 'mvn exec:java -Dexec.mainClass="com.example.App"'
+            }
+        }
+    }
+
+    post {
+        success {
+            echo ' Build and Test successful!'
+        }
+        failure {
+            echo ' Build failed!'
+        }
+    }
 }
--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,23 +3,51 @@ package com.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class App {
     public static void main(String[] args) {
 
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+	options.addArguments("--headless=new");
+	options.addArguments("--disable-gpu");
+	options.addArguments("--window-size=1920,1080");
 
-        // Open Practice Test Automation login page
-        driver.get("https://practicetestautomation.com/practice-test-login/");
-        driver.manage().window().maximize();
+	options.addArguments("--no-sandbox");
+	options.addArguments("--disable-dev-shm-usage");
+	options.addArguments("--remote-allow-origins=*");
+	options.setAcceptInsecureCerts(true);
+	options.addArguments("--ignore-certificate-errors");
 
-        // Enter username
-        driver.findElement(By.id("username")).sendKeys("student");
+        WebDriver driver = new ChromeDriver(options);
 
-        // Enter password
-        driver.findElement(By.id("password")).sendKeys("Password123");
+        try {
+            // Open website
+            driver.get("https://practicetestautomation.com/practice-test-login/");
 
-        // Click login button
-        driver.findElement(By.id("submit")).click();
+            // Enter username
+            driver.findElement(By.id("username")).sendKeys("student");
+
+            // Enter password
+            driver.findElement(By.id("password")).sendKeys("Password123");
+
+            // Click login
+            driver.findElement(By.id("submit")).click();
+
+            // Validation
+            String currentUrl = driver.getCurrentUrl();
+
+            if (currentUrl.contains("logged-in-successfully")) {
+                System.out.println("Login Successful");
+            } else {
+                System.out.println("Login Failed");
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            // Close browser
+            driver.quit();
+        }
     }
 }
